d7acd7bd4c04d15ab9ea88ede2341a1b9f1dc563
Vladimir Bukatov <bukatovvs@gmail.com>
2024-04-30 23:22:58 +0400

    test_selene and allure-results

---
+++ tests/test_selene.py
@@ -0,0 +1,11 @@
+from selene import browser, by, be
+from selene.support.shared.jquery_style import s
+
+
+def test_github_selene():
+    browser.open("https://www.github.com")
+    s(".search-input").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example").press_enter()
+    s(by.link_text("eroshenkoam/allure-example")).click()
+    s("#issues-tab").click()
+    s(by.text("Issue for HW qa.guru")).should(be.visible)
